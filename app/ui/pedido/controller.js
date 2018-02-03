6bb4496950e5b255f80542ea7ef97c6c5f592faf
Vinicius Sossella <vinicius.sossella@ilegra.com>
2018-02-03 13:33:50 -0200

    new logo - pedido

--- app/ui/pedido/controller.js
+++ app/ui/pedido/controller.js
@@ -11,24 +11,80 @@ angular.module('myApp.pedido', ['ngRoute'])
 
     .controller('PedidoCtrl', ['$scope', '$http', '$log', 'hexafy', function ($scope, $http, $log, hexafy) {
 
-        // Select first tab
-        // $('.nav-tabs a:first').tab('show') 
+        $('#myModal').modal('show')
 
-        // $scope.tab1 = function(a) {
-        //     $(a).tab('show');
-        //     alert('clico');
-        // };
+        $scope.rotas = []
+        $scope.pedidos = []
 
-        $(".nav-tabs a").click(function(){
-            $(this).tab('show');
-            return false;
-        });
+        var produtosList = [
+            {
+                id: 0,
+                name: "Rucula"
+            },
+            {
+                id: 1,
+                name: "Tomate"
+            },
+            {
+                id: 2,
+                name: "Alface"
+            }];
 
-        // $(".nav-tabs a:second").click(function(){
-        //     alert('clico');
-        //     $(this).tab('show');
-        // });
-        
+        var clientesList = [
+            {
+                id: 0,
+                name: "Cliente 0"
+            },
+            {
+                id: 1,
+                name: "Cliente 1"
+            },
+            {
+                id: 2,
+                name: "Cliente 2"
+            }];
 
+        $scope.tabNovaRotaClicked = function ($event) {
+            $('#myModal').modal('show')
+            $event.preventDefault()
+        }
+
+        $scope.choosenRota = function () {
+            unactiveTabs()
+            $scope.rotas.push({
+                name: $('#rotaSelect').val(),
+                produtos: produtosList,
+                clientes: clientesList,
+                quantidade: "",
+                selectedProduto: "",
+                selectedCliente: "",
+                pedidos: []
+            })
+            // var clild = $scope.rotas.length - 1;
+            // $('.nav-item a:nth(' + clild + ')').click();
+            $('#myModal').modal('hide')
+        }
+
+        $scope.addProduto = function(rotaName) {
+
+            var rotaInEdition = $scope.rotas.filter((rota) => rota.name === rotaName)[0]
+
+            rotaInEdition.pedidos.push({
+                cliente: rotaInEdition.selectedCliente,
+                produto: rotaInEdition.selectedProduto,
+                quantidade: rotaInEdition.quantidade
+            });
+            
+        }
+
+        $scope.tabClicked = function ($event) {
+            $($event.target).tab('show')
+            $event.preventDefault()
+        }
+
+        function unactiveTabs() {
+            $('.nav-item a').removeClass('active').removeClass('show');
+            $('.tab-content div').removeClass('active').removeClass('show');
+        }
 
     }]);
